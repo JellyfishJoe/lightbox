ccbf3cb77f4a79d4e50543613655686e92af456c
JellyfishJoe <jellyfishjoe5@gmail.com>
2018-10-17 15:11:52 -0700

    thingies

--- js/js.js
+++ js/js.js
@@ -16,6 +16,29 @@ $(document).ready(() => {
 
 		$('<div id="lightbox"></div>').hide().appendTo('body');
 
-		$('<img>').attr('src', $(this).attr('href')).on('load', () => /*function goes here*/).click(() => /*go away here*/).appendTo('#lightbox');
+		$('<img>').attr('src', $(this).attr('href')).on('load', () => placeImg()).click(() => remove()).appendTo('#lightbox');
+
+		$('#overlay').click(() => remove());
 	});
 });
+
+const placeImg = () => {
+	console.log('ran');
+	var top = ($(window).height() - $('#lightbox').height()) / 2;
+	var left = ($(window).width() - $('#lightbox').width()) / 2;
+	console.log(top, left);
+	$('#lightbox').css({
+		'top':top,
+		'left':left
+	}).fadeIn('slow');
+};
+
+const remove = () => {
+	$('#overlay, #lightbox').fadeOut('slow', function(){
+		$(this).remove();
+		$('body').css({
+			'overflow-y': 'auto' 
+		});
+	});
+
+}
