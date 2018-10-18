1d0514495ae5a0c50ac2b91d385609b1b8497088
JellyfishJoe <jellyfishjoe5@gmail.com>
2018-10-18 15:16:29 -0700

    fasda

--- js/js.js
+++ js/js.js
@@ -20,6 +20,29 @@ $(document).ready(() => {
 
 		$('#overlay').click(() => remove());
 	});
+
+	//Slideshow
+	$('#innerContainer').click(function(){
+		var scroolAmount = $(this).width() - 15 - $(this).parent().width();
+		console.log(`scroolAmount = ${scroolAmount}`);
+		var currentPos = Math.abs(parseInt($(this).css('left')));
+		console.log(`currentPos = ${currentPos}`);
+		var nextScrool = ~~($(this).parent().width());
+		console.log(`nexrScrool = ${nextScrool}`);
+		var remainingScrool = scroolAmount - currentPos;
+		console.log(`remainingScrool = ${remainingScrool}`);
+
+		currentPos < scroolAmount ? $(this).animate({'left': '-=' + nextScrool}, 'slow') : $(this).animate({'left': '0'}, 'fast');
+	})
+
+	//Crossfade
+	$('#crossfade').hover(function(){
+		var pos = $(this).offset();
+		console.log(pos.top);
+		$(this).find('img:eq(1)').stop(true, true).fadeIn().css({});
+	}, function(){
+		$(this).find('img:eq(1)').fadeOut();
+	})
 });
 
 const placeImg = () => {
